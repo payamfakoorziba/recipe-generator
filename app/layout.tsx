6849b2f1613725451d0117e223da0c248f3c6b7c
Payam Fakoorziba <payamfakoor@gmail.com>
2024-08-26 21:51:17 -0400

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "AI Recipe Generator",
@@ -14,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={GeistSans.className}>{children}</body>
+      <body className={GeistSans.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
